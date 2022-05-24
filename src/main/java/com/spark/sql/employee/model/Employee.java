14bea0c1ac9ba1b3c14151c43c10da60ce3946ad
Vaishnavi Aloor <vaishnavialoor@gmail.com>
2022-05-24 15:21:20 +0530

    lombok added

--- src/main/java/com/spark/sql/employee/model/Employee.java
+++ src/main/java/com/spark/sql/employee/model/Employee.java
@@ -1,62 +1,15 @@
 package com.spark.sql.employee.model;
 
-import java.util.Date;
+import lombok.*;
 
+
+@Data
 public class Employee {
-    private int emp_id;
+    private  int emp_id;
     private String emp_name;
-    private int emp_salary;
-  //  private Date emp_join_date;
-    private int emp_loc_id;
+    private  int emp_salary;
+  //  private  Date emp_join_date;
+    private  int emp_loc_id;
 
-    public int getEmp_id() {
-        return emp_id;
-    }
 
-    public void setEmp_id(int emp_id) {
-        this.emp_id = emp_id;
-    }
-
-    public String getEmp_name() {
-        return emp_name;
-    }
-
-    public void setEmp_name(String emp_name) {
-        this.emp_name = emp_name;
-    }
-
-    public int getEmp_salary() {
-        return emp_salary;
-    }
-
-    public void setEmp_salary(int emp_salary) {
-        this.emp_salary = emp_salary;
-    }
-
-    /*public Date getEmp_join_date() {
-        return emp_join_date;
-    }
-
-    public void setEmp_join_date(Date emp_join_date) {
-        this.emp_join_date = emp_join_date;
-    }*/
-
-    public int getEmp_loc_id() {
-        return emp_loc_id;
-    }
-
-    public void setEmp_loc_id(int emp_loc_id) {
-        this.emp_loc_id = emp_loc_id;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "emp_id=" + emp_id +
-                ", emp_name='" + emp_name + '\'' +
-                ", emp_salary=" + emp_salary +
-               /* ", emp_join_date=" + emp_join_date +*/
-                ", emp_loc_id=" + emp_loc_id +
-                '}';
-    }
 }
--- src/main/java/com/spark/sql/employee/vo/emp_loc_join.java
+++ src/main/java/com/spark/sql/employee/vo/emp_loc_join.java
@@ -1,5 +1,8 @@
 package com.spark.sql.employee.vo;
 
+import lombok.*;
+
+@Data
 public class emp_loc_join {
     private int emp_id;
     private String emp_name;
@@ -9,51 +12,5 @@ public class emp_loc_join {
     private String loc_name;
     private int loc_id;
 
-    public int getEmp_id() {
-        return emp_id;
-    }
 
-    public void setEmp_id(int emp_id) {
-        this.emp_id = emp_id;
-    }
-
-    public String getEmp_name() {
-        return emp_name;
-    }
-
-    public void setEmp_name(String emp_name) {
-        this.emp_name = emp_name;
-    }
-
-    public int getEmp_salary() {
-        return emp_salary;
-    }
-
-    public void setEmp_salary(int emp_salary) {
-        this.emp_salary = emp_salary;
-    }
-
-    public int getEmp_loc_id() {
-        return emp_loc_id;
-    }
-
-    public void setEmp_loc_id(int emp_loc_id) {
-        this.emp_loc_id = emp_loc_id;
-    }
-
-    public String getLoc_name() {
-        return loc_name;
-    }
-
-    public void setLoc_name(String loc_name) {
-        this.loc_name = loc_name;
-    }
-
-    public int getLoc_id() {
-        return loc_id;
-    }
-
-    public void setLoc_id(int loc_id) {
-        this.loc_id = loc_id;
-    }
 }
